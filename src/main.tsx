48845a1e2733ef05a7865daeac32db2de328984b
KyP+AnhT <anh.q.tran@eastagile.com>
2024-02-12 10:51:39 +0700

    [c] Add css variables to dev mode
    Co-authored-by: Ky Phan <ky.phan@eastagile.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// Import all dependencies
 import App from 'App'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -5,17 +6,27 @@ import './App.scss'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AuthProvider from './providers/auth'
 
-const queryClient = new QueryClient()
-const container = document.querySelector('#root')
-if (container) {
-  const root = createRoot(container)
-  root.render(
-    <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </QueryClientProvider>
-    </StrictMode>
-  )
+async function importDevStylesheet() {
+  if (import.meta.env.DEV) {
+    await import('./App.dev.scss')
+  }
 }
+
+function createAndRenderApp(container: Element | null) {
+  if (container) {
+    const queryClient = new QueryClient()
+    const root = createRoot(container)
+    root.render(
+      <StrictMode>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </QueryClientProvider>
+      </StrictMode>
+    )
+  }
+}
+
+void importDevStylesheet()
+createAndRenderApp(document.querySelector('#root'))
