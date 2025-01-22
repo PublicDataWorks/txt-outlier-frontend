005455afe71d2a3393741a388048dc1ac4c7c5dc
Thuan Ngo <thuan.ngolegia@gmail.com>
2025-01-22 17:05:44 +0700

    [c] - Clean up the old code

--- cypress/support/commands.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/cypress/add-commands'
-
-Cypress.Commands.add('seed', () => {
-  cy.intercept('GET', 'http://localhost:8000/functions/v1/backend/broadcasts', {
-    fixture: 'broadcasts.json'
-  }).as('seed')
-  cy.visit('/')
-  cy.wait('@seed')
-})
--- src/utils/removeExtraSpaces.ts
+++
@@ -1,3 +0,0 @@
-const removeExtraSpaces = (str: string) => str.replace(/\s\s+/g, ' ')
-
-export default removeExtraSpaces
