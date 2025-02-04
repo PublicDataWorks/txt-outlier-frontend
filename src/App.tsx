3bca5f6e85431be7475ed5254e2550f744906d8f
Thuan Ngo <thuan.ngolegia@gmail.com>
2025-02-04 15:07:44 +0700

    [f] - Last batch UI
    Co-authored-by: Ky Phan <ky.phan@eastagile.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import LastBatchSection from '@/components/LastBatchSection';
 import NextBatchSection from '@/components/NextBatchSection';
 import { ScrollArea } from '@/components/ui/scroll-area';
 
@@ -22,6 +23,7 @@ function App() {
       <ScrollArea className="h-screen w-full border-l bg-background dark:bg-[#242424] dark:border-l-neutral-800 dark">
         <div className="space-y-6 p-4">
           <NextBatchSection />
+          <LastBatchSection />
         </div>
       </ScrollArea>
       <ReactQueryDevtools initialIsOpen={false} />
